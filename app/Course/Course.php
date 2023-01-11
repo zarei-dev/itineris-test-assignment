ebccbf53e764f45a53d45ce57bf9559c05628169
Mohammad Zarei <mohammad.zarei1380@gmail.com>
2023-01-12 00:36:08 +0330

    feat: Add course_type and campus taxonomy

--- app/Course/Course.php
+++ app/Course/Course.php
@@ -4,6 +4,7 @@ namespace ITINERIS_ASSIGNMENT\App\Course;
 class Course {
     public function __construct() {
         add_action( 'init', array( $this, 'register_post_type' ) );
+        add_action( 'init', array( $this, 'register_taxonomies' ) );
 
         $MetaBoxes = new MetaBoxes();
     }
@@ -11,11 +12,54 @@ class Course {
     public function register_post_type() {
         register_extended_post_type( 'course', [
             'show_in_feed' => true,
-            'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+            'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+            'admin_cols' => [
+                'course_type' => [
+                    'taxonomy' => 'course_type'
+                ],
+                'campus' => [
+                    'taxonomy' => 'campus'
+                ],
+            ],
+            'admin_filters' => [
+                'course_type' => [
+                    'taxonomy' => 'course_type'
+                ],
+                'campus' => [
+                    'taxonomy' => 'campus'
+                ],
+            ],
         ], [
             'singular' => 'Course',
             'plural'   => 'Courses',
             'slug'     => 'courses',
+        ] );
+    }
+
+    public function register_taxonomies() {
+        // register course type and campus taxonomy
+        register_extended_taxonomy( 'course_type', 'course', [
+            'show_in_feed' => true,
+            'show_admin_column' => true,
+            'show_in_rest' => true,
+            'meta_box' => 'radio',
+            'hierarchical' => true,
+        ], [
+            'singular' => 'Course Type',
+            'plural'   => 'Course Types',
+            'slug'     => 'course-types',
+        ] );
+
+        register_extended_taxonomy( 'campus', 'course', [
+            'show_in_feed' => true,
+            'show_admin_column' => true,
+            'show_in_rest' => true,
+            'meta_box' => 'radio',
+            'hierarchical' => true,
+        ], [
+            'singular' => 'Campus',
+            'plural'   => 'Campuses',
+            'slug'     => 'campuses',
         ] );
     }
 }
