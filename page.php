fc876c5504d2ac5825551f9c54cf8e77edecf91f
Mohammad Zarei <mohammad.zarei1380@gmail.com>
2023-01-16 22:45:11 +0330

    feat: Add page template

---
+++ page.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?php echo wp_title(); ?></title>
+        <meta charset="utf-8">
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <?php the_content() ?>
+
+        <?php 
+        if ( is_front_page() ) {
+            get_template_part('template_parts/courses_filter');
+        }
+        ?>
+
+        <?php wp_footer(); ?>
+    </body>
+</html>
