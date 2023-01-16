614514a11c34cbf89b6b6b8d9eaae5cd53964ef6
Mohammad Zarei <mohammad.zarei1380@gmail.com>
2023-01-17 00:21:19 +0330

    fix: Page title

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>WP Starter Theme</title>
+        <title><?php wp_title(); ?></title>
         <meta charset="utf-8">
         <?php wp_head(); ?>
     </head>
     <body>
-        <h1 class="text-xl text-center">WP Starter Theme</h1>
-
         <?php get_template_part('template_parts/quote_with_image'); ?>
         <?php get_template_part('template_parts/courses_filter'); ?>
 
